f9fe04d50ccfa0b775b6bbd9d522c3abd36409c9
himanshu <himanshub1295@gmail.com>
2024-11-01 15:44:14 +0530

    Email Validator

---
+++ lib/email_validator/email_validator.dart
@@ -0,0 +1,9 @@
+// EMAIL FORMAT VALIDATOR
+bool isValidEmail(String email) {
+  // Regular expression pattern for email validation
+  final RegExp emailRegex = RegExp(
+    r'^[\w-]+(\.[\w-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,})$',
+  );
+
+  return emailRegex.hasMatch(email);
+}
