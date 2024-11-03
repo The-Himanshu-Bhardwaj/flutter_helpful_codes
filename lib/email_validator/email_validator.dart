2ba428ff21590eafbe83a0d021e896c3a82da223
himanshu <himanshub1295@gmail.com>
2024-11-03 15:30:27 +0530

    Singleton Shared Preferences

--- lib/email_validator/email_validator.dart
+++ lib/email_validator/email_validator.dart
@@ -4,6 +4,5 @@ bool isValidEmail(String email) {
   final RegExp emailRegex = RegExp(
     r'^[\w-]+(\.[\w-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,})$',
   );
-
   return emailRegex.hasMatch(email);
 }
